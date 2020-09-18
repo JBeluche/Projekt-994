df7cc96cfa2460f236844bfb112daf30a4e53ec3
Jeremie <j.beluche@outlook.com>
2020-09-18 12:49:27 +0200

    Added 3th person mesh to CharacterBase

--- Source/Projekt994/Projekt994GameMode.cpp
+++ Source/Projekt994/Projekt994GameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "Projekt994GameMode.h"
 #include "Projekt994HUD.h"
-#include "Projekt994Character.h"
+#include "Projekt994/Public/Player/CharacterBase.h"
 #include "UObject/ConstructorHelpers.h"
 
 AProjekt994GameMode::AProjekt994GameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/BlueprintClasses/Player/BP_CharacterBase.BP_CharacterBase"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/BlueprintClasses/Player/BP_CharacterBase.BP_CharacterBase_C"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
