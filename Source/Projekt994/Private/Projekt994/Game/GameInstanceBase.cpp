75a6774ce77eeab723c6e2ac07339e44e2399514
Jeremie <j.beluche@outlook.com>
2020-09-21 11:16:26 +0200

    Made GameInstanceBase to create map info struct and use it in the User iterface widget

---
+++ Source/Projekt994/Private/Projekt994/Game/GameInstanceBase.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projekt994/Public/Projekt994/Game/GameInstanceBase.h"
+
+UGameInstanceBase::UGameInstanceBase()
+{
+
+}
---
+++ Source/Projekt994/Public/Projekt994/Game/GameInstanceBase.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GameInstanceBase.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FMapInfo
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FString MapURL;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FString MapName;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FString MapDescription;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		class UTexture2D* MapImage;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class PROJEKT994_API UGameInstanceBase : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	public:
+
+	UGameInstanceBase();
+
+
+};
