70274baff21fa793e8007e1501f6f315f111b302
Jeremie <j.beluche@outlook.com>
2020-10-09 15:02:14 +0200

    Created zombie base class, added nav mesh

---
+++ Source/Projekt994/Private/Projekt994/Zombie/ZombieBase.cpp
@@ -0,0 +1,17 @@
+// Copyright by Creating Mountains
+
+
+#include "Projekt994/Public/Projekt994/Zombie/ZombieBase.h"
+
+// Sets default values
+AZombieBase::AZombieBase()
+{
+
+}
+
+// Called when the game starts or when spawned
+void AZombieBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/Projekt994/Public/Projekt994/Zombie/ZombieBase.h
@@ -0,0 +1,25 @@
+// Copyright by Creating Mountains
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ZombieBase.generated.h"
+
+UCLASS()
+class PROJEKT994_API AZombieBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AZombieBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+
+
+};
